ef46f3d6f136a5a221be113c6e76102d5fc754d2
Beltran <beltran.bulbarella@ing.austral.edu.ar>
2021-03-22 20:58:27 -0300

    a

--- src/Trabajo1/Guide1RecursiveSolution.java
+++ src/Trabajo1/Guide1RecursiveSolution.java
@@ -1,21 +1,25 @@
 package Trabajo1;
+import java.util.*;
 
 public class Guide1RecursiveSolution implements Guide1 {
     @Override
     public int exercise_1_d(int n) {
+        if (n > 0){
+            return (2*n) - 1 + exercise_1_d(n - 1);
+        }
         return 0;
     }
+
 
     @Override
     public int exercise_2_c(int n) {
         if (n > 1) {
-            return exercise_2_c(n - 1) + exercise_2_c(n - 2);  //función recursiva
-        } else if (n == 1) {  // caso base
+            return exercise_2_c(n - 1) + exercise_2_c(n - 2);
+        } else if (n == 1) {
             return 1;
-        } else if (n == 0) {  // caso base
+        } else if (n == 0) {
             return 0;
-        } else { //error
-            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+        } else {
             return -1;
         }
     }
